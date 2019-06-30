d2b5efaf192ca1047ba3df6b5699b3d77e272d06
Sen Gupta <sen@icans.com>
2019-06-30 13:50:49 +0530

    added prefetch

--- serviceworker.js
+++ serviceworker.js
@@ -1,8 +1,15 @@
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init();
+
 AMP_SW.init({
     offlinePageOptions: {
         url: '/offline.html',
         assets: ['/images/offline-header.jpg']
     }
-  })
+  });
+  AMP_SW.init({
+    assetCachingOptions: [{
+         regexp: /\.(png|jpg)/,
+         cachingStrategy: 'CACHE_FIRST'
+     }],
+    });
