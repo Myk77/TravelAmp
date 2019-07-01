cb7ee793bfb15711e901bee2aeff1940c9130c83
ICANS\vivek <kumar.vivek@ictpvtltd.com>
2019-07-01 14:04:06 +0530

    added listener

--- serviceworker.js
+++ serviceworker.js
@@ -6,5 +6,31 @@ AMP_SW.init({
         url: './offline.html',
         assets: ['./images/offline-header.jpg']
     }
-  });
+});
+var CACHE_NAME = 'travelamp-v1';
+var urlsToCache = [
+    '/',
+    '/img/',
+    '/css/'
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+      event.respondWith(fetch('/pwa'));
+
+      // Immediately start downloading the actual resource.
+      fetch(event.request.url);
+    }
+
+});
  
