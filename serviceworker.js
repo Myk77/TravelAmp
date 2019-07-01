673d0731875f64ad76beff0b1d96b7bf72a6a0d1
ICANS\vivek <kumar.vivek@ictpvtltd.com>
2019-07-01 16:37:35 +0530

    sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,36 +1,39 @@
-//importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
-//AMP_SW.init();
+importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+AMP_SW.init();
 
-//AMP_SW.init({
-//    offlinePageOptions: {
-//        url: './offline.html',
-//        assets: ['./images/offline-header.jpg']
-//    }
-//});
-var CACHE_NAME = 'travelamp-v1';
-var urlsToCache = [
-    '/',
-    '/img/',
-    '/css/'
-];
-
-self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-self.addEventListener('fetch', event => {
-    if (event.request.mode === 'navigate') {
-      event.respondWith(fetch('/pwa'));
-
-      // Immediately start downloading the actual resource.
-      fetch(event.request.url);
+AMP_SW.init({
+    offlinePageOptions: {
+        url: './offline.html',
+        assets: ['./images/offline-header.jpg']
     }
+});
+AMP_SW.init(function () {
+
+    var CACHE_NAME = 'travelamp-v1';
+    var urlsToCache = [
+        '/',
+        '/img/',
+        '/css/'
+    ];
+
+    self.addEventListener('install', function (event) {
+        // Perform install steps
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function (cache) {
+                    console.log('Opened cache');
+                    return cache.addAll(urlsToCache);
+                })
+        );
+    });
+    self.addEventListener('fetch', event => {
+        if (event.request.mode === 'navigate') {
+            event.respondWith(fetch('/pwa'));
+
+            // Immediately start downloading the actual resource.
+            fetch(event.request.url);
+        }
+
+    });
 
 });
- 
